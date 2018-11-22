e8d3ae1fa10a9f03e14833a86d64630c50ade612
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-22 20:34:37 +0000

    Deploy EVStarostin/travis to github.com/EVStarostin/travis.git:gh-pages

---
+++ precache-manifest.2feaf18eb4beb200386aecf74bc68549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8909b61bc84d0533dc0c",
+    "url": "/travis/static/css/main.3feecfd6.chunk.css"
+  },
+  {
+    "revision": "8909b61bc84d0533dc0c",
+    "url": "/travis/static/js/main.8909b61b.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/travis/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "24685b084b5337dcb2b7",
+    "url": "/travis/static/js/runtime~main.24685b08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91180c4430099cf32d31e5327ab5eeac",
+    "url": "/travis/index.html"
+  }
+];
